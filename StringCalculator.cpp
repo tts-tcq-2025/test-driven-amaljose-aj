4e06ab98e37edbe409fb3a86f62c20d6680ae5ac
amaljose-aj <57029243+amaljose-aj@users.noreply.github.com>
2025-10-15 11:15:11 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,6 +1,7 @@
 #include "StringCalculator.h"
+#include <string>  // added for cpplint compliance
 
 int StringCalculator::Add(const std::string& /*numbers*/) {
-    // minimal code to pass the first test
-    return 0;
+  // minimal code to pass the first test
+  return 0;
 }
